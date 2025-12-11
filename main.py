c0a60a19742ea3cb7e9ca37bc5eee2f6daf582ba
Omkar Sathish <omkarsathish06@gmail.com>
2025-12-11 12:21:35 +0530

    feat: async main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 from dotenv import load_dotenv
 from langchain_qdrant import QdrantVectorStore
 from langchain_openai.embeddings import OpenAIEmbeddings
@@ -6,10 +6,11 @@ from typing_extensions import TypedDict
 from typing import List
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.mongodb import MongoDBSaver
+import asyncio
 
 load_dotenv()
 
-llm = OpenAI()
+llm = AsyncOpenAI()
 
 
 class State(TypedDict):
@@ -29,9 +30,9 @@ vector_db = QdrantVectorStore.from_existing_collection(
 )
 
 
-def search_node(state: State):
+async def search_node(state: State):
     user_query = state["messages"][-1]["content"]
-    search_results = vector_db.similarity_search(query=user_query)
+    search_results = await vector_db.asimilarity_search(query=user_query, k=10)
     context = "\n\n".join(
         [
             f"Page Content: {result.page_content}\n Page Number: {result.metadata['page_label']}\nFile Location:{result.metadata['source']}"
@@ -42,7 +43,7 @@ def search_node(state: State):
     return state
 
 
-def llm_node(state: State):
+async def llm_node(state: State):
     SYSTEM_PROMPT = f"""
         You are a helpful AI Assistant who answers user queries based on the available context retrieved from a PDF file
         along with page contents and page number.
@@ -52,7 +53,7 @@ def llm_node(state: State):
         {state["context"]}
     """
     state["messages"].append({"role": "system", "content": SYSTEM_PROMPT})
-    llm_response = llm.chat.completions.create(
+    llm_response = await llm.chat.completions.create(
         model="gpt-4.1-mini", messages=state["messages"]
     )
     system_msg = {"role": "system", "content": llm_response.choices[0].message.content}
@@ -60,21 +61,21 @@ def llm_node(state: State):
     return state
 
 
-def validate_result(state: State):
+async def validate_result(state: State):
     VALIDATE_RESULT_PROMPT = f"""
     You are an AI Assistant which helps in analyzing the user query and the respective llm response.
     Your goal is to check how relevant the answer is and grade it from 0-10. A grade of 0 signifies
     no relevance between the user query and llm response while 10 signifies total/complete relevance
     between the user query and llm response.
-    
+
     Your response should only be a single digit and that single digit should be the rating.
-    
+
     CONTEXT:
-    
+
     user query: {state["messages"][-2]["content"]}
-    llm response: {state["messages"][-1]["content"]}    
+    llm response: {state["messages"][-1]["content"]}
     """
-    llm_response = llm.chat.completions.create(
+    llm_response = await llm.chat.completions.create(
         model="gpt-4.1",
         messages=[{"role": "system", "content": VALIDATE_RESULT_PROMPT}],
     )
@@ -116,21 +117,28 @@ graph.set_entry_point("search")
 URI = "mongodb://:27017"
 config = {"configurable": {"thread_id": "1"}}
 
-with MongoDBSaver.from_conn_string(URI) as checkpointer:
-    rag_chain = create_memory_graph(graph, checkpointer=checkpointer)
 
-    while True:
-        user_query = input(">> ")
-        user_msg = {"role": "user", "content": user_query}
-        rag_state: State = {
-            "messages": [user_msg],
-            "context": "",
-            "run_count": 0,
-            "relevance_score": None,
-        }
-        final_state = rag_chain.invoke(rag_state, config)
-        if final_state["run_count"] >= 3 and final_state["relevance_score"] < 5:
-            print(
-                "Could not generate a highly relevant answer after 3 attempts. Here is the best attempt:"
-            )
-        print(final_state["messages"][-1]["content"])
+async def main():
+    """Main async function to run the RAG system"""
+    with MongoDBSaver.from_conn_string(URI) as checkpointer:
+        rag_chain = create_memory_graph(graph, checkpointer=checkpointer)
+
+        while True:
+            user_query = input(">> ")
+            user_msg = {"role": "user", "content": user_query}
+            rag_state: State = {
+                "messages": [user_msg],
+                "context": "",
+                "run_count": 0,
+                "relevance_score": None,
+            }
+            final_state = await rag_chain.ainvoke(rag_state, config)
+            if final_state["run_count"] >= 3 and final_state["relevance_score"] < 5:
+                print(
+                    "Could not generate a highly relevant answer after 3 attempts. Here is the best attempt:"
+                )
+            print(final_state["messages"][-1]["content"])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
